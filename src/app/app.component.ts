9f3f68f0e9a99f7c3ef81d6308f512cd959d1439
FabianAlvarado <fabian.rhcp@gmail.com>
2017-04-02 09:18:12 -0500

    Inital configs done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title: string = 'Welcome to my practice App!';
+    subtitle: string = `The purpose of this app is not to explain the concepts and theory of the coding on this 
+    technologies, please refer to the subject matter experts to learn from them. It is instead meant to help you to practice 
+    the sintaxis of the expressions as most likely we do not practice all we learned in a daily basis, so take a 
+    few minutes to refresh all knowledge it costed blood sweat and tears to gain and challenge you to do it faster and better everytime`;
 }
