1df6ba677b9afdaefc464393b6442968d2aca3c6
FabianAlvarado <fabian.rhcp@gmail.com>
2017-04-04 17:03:02 -0500

    Duplicate func. done, starting to simplify

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,60 +1,96 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn, FormArray } from '@angular/forms';
 
-import { Customer } from './customer';
+import 'rxjs/add/operator/debounceTime';
 
+//import { Customer } from './customer';
 
-function fieldsMatcher(c: AbstractControl) {
+function fieldMatcher(c: AbstractControl): { [key: string]: boolean } | null {
     let emailControl = c.get('email');
     let confirmControl = c.get('confirmEmail');
-
     if (emailControl.pristine || confirmControl.pristine) {
         return null;
     }
     if (emailControl.value === confirmControl.value) {
         return null;
     }
-    return { 'match' : true };
+    return { 'match': true };
 }
 
-
-
-
-function ratingRange(min: number, max: number): ValidatorFn { // FACTORY FUNCTION
+function ratingRange(min: number, max: number): ValidatorFn {
     return (c: AbstractControl): { [key: string]: boolean } | null => {
         if (c.value !== undefined && (isNaN(c.value) || c.value < min || c.value > max)) {
             return { 'range': true };
         };
         return null;
-    }
+    };
 }
-
-
 
 @Component({
     moduleId: module.id,
     selector: 'app-questions-component',
     templateUrl: 'customer.component.html'
 })
-export class CustomerComponent implements OnInit {
-    customerForm: FormGroup; // We will refer to this in the template: [formGroup]="customerForm"
-    customer: Customer = new Customer(); /* Manages the instance of the customer data we are binding to our template
-    [I need to replace with the service]*/
 
-    constructor(private myFormBuilder: FormBuilder) { }
+export class CustomerComponent implements OnInit {
+    customerForm: FormGroup;
+    //customer: Customer = new Customer();
+    emailMessage: string;
+
+    get addresses(): FormArray {
+        return <FormArray>this.customerForm.get('addresses');
+    }
+
+    private validationMessages = {
+        required: 'Please enter your email address.',
+        pattern: 'Please enter a valid email address.'
+    };
+
+    constructor(private fb: FormBuilder) { }
 
     ngOnInit(): void {
-        this.customerForm = this.myFormBuilder.group({
-            firstName: ['', [Validators.required, Validators.minLength(7)]],
-            lastName: ['', [Validators.required, Validators.maxLength(10)]],
-            emailGroup: this.myFormBuilder.group({
-                email: ['', [Validators.required, Validators.email]],
-                confirmEmail: ['', Validators.required]
-            }, { validator: fieldsMatcher }),
+        this.customerForm = this.fb.group({
+            firstName: ['', [Validators.required, Validators.minLength(3)]],
+            lastName: ['', [Validators.required, Validators.maxLength(50)]],
+            emailGroup: this.fb.group({
+                email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+')]],
+                confirmEmail: ['', Validators.required],
+            }, { validator: fieldMatcher }),
             phone: '',
             notification: 'email',
             rating: ['', ratingRange(1, 5)],
             sendCatalog: true,
+            addresses: this.fb.array([this.buildAddress()])
+        });
+
+        this.customerForm.get('notification').valueChanges
+            .subscribe(value => this.setNotification(value));
+
+        const emailControl = this.customerForm.get('emailGroup.email');
+        emailControl.valueChanges.debounceTime(1000).subscribe(value =>
+            this.setMessage(emailControl));
+    }
+
+    addAddress(): void {
+        this.addresses.push(this.buildAddress());
+    }
+
+    buildAddress(): FormGroup {
+        return this.fb.group({
+            addressType: 'home',
+            street1: '',
+            street2: '',
+            city: '',
+            state: '',
+            zip: ''
+        });
+    }
+
+    populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            lastName: 'Harkness',
+            sendCatalog: false
         });
     }
 
@@ -63,15 +99,14 @@ export class CustomerComponent implements OnInit {
         console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
 
-    populateTestData(): void {
-
-        this.customerForm.patchValue({
-            firstName: "Jack",
-            lastName: "Sparrow",
-            email: "caribean@pirates.com",
-            sendCatalog: false
-        });
+    setMessage(c: AbstractControl): void {
+        this.emailMessage = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.emailMessage = Object.keys(c.errors).map(key =>
+                this.validationMessages[key]).join(' ');
+        }
     }
+
     setNotification(notifyVia: string): void {
         const phoneControl = this.customerForm.get('phone');
         if (notifyVia === 'text') {
