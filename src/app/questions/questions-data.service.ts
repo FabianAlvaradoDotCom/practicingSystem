1c014998457a2a5afdbf8cc909d1c4caa486ccbf
Fabian Alvarado <fabian.rhcp@gmail.com>
2017-04-29 21:13:14 -0500

    Code format validation fixed, it accepts multi br and sps

--- src/app/questions/questions-data.service.ts
+++ src/app/questions/questions-data.service.ts
@@ -12,7 +12,7 @@ export class QuestionsDataService {
                 "question": "Cannot start with a number, cannot have spaces, reserved words nor characters except \"$\" \"_\", the variable...",
                 "answer": "name",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer"
@@ -24,7 +24,7 @@ export class QuestionsDataService {
                 "question": "Prints a console  a warning",
                 "answer": "console.warn();",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer"
@@ -36,7 +36,7 @@ export class QuestionsDataService {
                 "question": "Prints a console error",
                 "answer": "console.error();",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer"
@@ -48,7 +48,7 @@ export class QuestionsDataService {
                 "question": "Data types that represent a single value, meaning it is not an object.",
                 "answer": "Primitive Data Types",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "____ Data Types",
                 "errorMessage": "Wrong answer, verify capital letters"
@@ -60,7 +60,7 @@ export class QuestionsDataService {
                 "question": "Mention the Primitive Data Types alphabetically ordered",
                 "answer": "Boolean, Null, Number, String, Symbol, Undefined",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Tip, there are 6"
@@ -72,7 +72,7 @@ export class QuestionsDataService {
                 "question": "A special function syntactically differently that generally takes two parameters and returns one result. It uses the \"infix notation\" which means that the operator is placed between the 2 values to process.",
                 "answer": "Operator",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer"
@@ -84,7 +84,7 @@ export class QuestionsDataService {
                 "question": "Write the code to print in the console a Random Number",
                 "answer": "console.log(Math.random());",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer"
@@ -96,7 +96,7 @@ export class QuestionsDataService {
                 "question": "Write the code to Round and print in the console this number: 8.5",
                 "answer": "console.log(Math.round(8.5));",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer"
@@ -108,7 +108,7 @@ export class QuestionsDataService {
                 "question": "Mention 7 Host Objects alphabetically ordered",
                 "answer": "Comment, Console, Document, Element, Event, Node, Window",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Tip: C, C, D, E, E, N, W"
@@ -120,7 +120,7 @@ export class QuestionsDataService {
                 "question": "Mention 7 Core Objects alphabetically ordered",
                 "answer": "Array, Boolean, Date, Math, Number, Object, String",
                 "id": "",
-                "rows": 3,
+                "rows": 2,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Tip: A, B, D, M, N, O, S"
@@ -130,9 +130,9 @@ export class QuestionsDataService {
                 "questionTopic": "Basics",
                 "class": "bdg4",
                 "question": "Write the code to compare through a Switch statement the var \"varCompare\" against 2 values to print in the console: \"Option1\" or printing \"Option2\" or a third option as default, printing in the console \"Option3\"",
-                "answer": "var varCompare = 10; switch(varCompare){ case 5: console.log(\"Option1\"); break; case 9: console.log(\"Option2\"); break; default: console.log(\"Option3\"); break; }",
+                "answer": "var varCompare = 10; \nswitch (varCompare) { \ncase 5: \nconsole.log(\"Option1\"); \nbreak; \ncase 9: \nconsole.log(\"Option2\"); \nbreak; \ndefault: \nconsole.log(\"Option3\"); \nbreak; \n}",
                 "id": "",
-                "rows": 8,
+                "rows": 12,
                 "type": "",
                 "placeholder": "",
                 "errorMessage": "Wrong answer."
--- src/app/questions/questions.component.ts
+++ src/app/questions/questions.component.ts
@@ -4,18 +4,35 @@ import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl, Valid
 import { IQuestionAnswer } from './question-answer'; // DataService Structure defined
 import { QuestionsDataService } from './questions-data.service'; // Importing actual data for questions
 
-
 function answerValidation(toCheck: string): ValidatorFn {  // Function with parameters
-   
+
     return (c: AbstractControl): { [key: string]: boolean } | null => {
 
-        if (c.value.replace(/\r?\n|\r/g, " ") !== toCheck.replace(/\r?\n|\r/g, " ")) {
-          
+        if ((c.value.replace(/\r?\n|\r/g, " ").split(" ").join("") !== toCheck.replace(/\r?\n|\r/g, " ").split(" ").join("")) || (cleanSpace(c.value).split(" ").length !== cleanSpace(toCheck).split(" ").length)) {
             return { 'match': true };
-        };        
+        };
         return null;
     };
-}
+};
+
+function cleanSpace(val): string {
+    val = val.replace(/\r?\n|\r/g, " ");
+    val = val.replace(/               /g, " ");
+    val = val.replace(/              /g, " ");
+    val = val.replace(/             /g, " ");
+    val = val.replace(/            /g, " ");
+    val = val.replace(/           /g, " ");
+    val = val.replace(/          /g, " ");
+    val = val.replace(/         /g, " ");
+    val = val.replace(/        /g, " ");
+    val = val.replace(/       /g, " ");
+    val = val.replace(/      /g, " ");
+    val = val.replace(/     /g, " ");
+    val = val.replace(/    /g, " ");
+    val = val.replace(/   /g, " ");
+    val = val.replace(/  /g, " ");
+    return val;
+};
 
 @Component({
     moduleId: module.id,
@@ -30,14 +47,14 @@ export class QuestionsComponent implements OnInit {
 
     get arrayAnswers(): FormArray {
         return <FormArray>this.questionsForm.get('arrayAnswers');
-    }    
+    }
 
     constructor(private myFormBuilder: FormBuilder, private _questionsAnswers: QuestionsDataService) { }// DataService initialized
 
     ngOnInit(): void {
         this.questionsAnswers = this._questionsAnswers.getQuestionAnswer();///////////////
 
-        this.questionsForm = this.myFormBuilder.group({            
+        this.questionsForm = this.myFormBuilder.group({
             displayQuestions: true,
             arrayAnswers: this.myFormBuilder.array([this.buildAnswer(this.questionsAnswers[0].answer)])
         });
@@ -46,12 +63,12 @@ export class QuestionsComponent implements OnInit {
 
     addAnswer(): void {
         for (let f = 1; f < this.questionsAnswers.length; f++) {
-            this.arrayAnswers.push(this.buildAnswer(this.questionsAnswers[f].answer));       
+            this.arrayAnswers.push(this.buildAnswer(this.questionsAnswers[f].answer));
         }
     }
 
-    buildAnswer(valor:any): FormGroup {
-        return this.myFormBuilder.group({           
+    buildAnswer(valor: any): FormGroup {
+        return this.myFormBuilder.group({
             answerField: ['', answerValidation(valor)],
             correctAnswerField: ''
         });
@@ -61,7 +78,7 @@ export class QuestionsComponent implements OnInit {
         console.log('Saved: ' + JSON.stringify(this.questionsForm.value));
     }
 
-    
+
     showAnswer(received) {
         received["mostrar"] = !received["mostrar"];
     }
